ff1a4bf294007fc764938c0c6f44f6467d9aa5de
Samuel <ingjezdlozcorp@gmail.com>
2024-05-21 11:47:43 -0400

    ios implementation

--- ios/Plugin/QualtricsAndroidPlugin.swift
+++ ios/Plugin/QualtricsAndroidPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import Qualtrics
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -14,5 +15,33 @@ public class QualtricsAndroidPlugin: CAPPlugin {
         call.resolve([
             "value": implementation.echo(value)
         ])
+    }
+
+    @objc public func initializeQualtricsWithParams(_ call: CAPPluginCall){
+        print("Hola samuel encuestas on qualtrics")
+        let value = call.getString("value") ?? ""
+        //Qualtrics.shared.initializeProject(brandId: "claropuertorico", projectId: "ZN_aaxt7pZtTL1fNzw", extRefId: "EXT_REF_ID", completion: { (myInitializationResult) in print(myInitializationResult);})
+        call.resolve(
+            [
+                    "value": value
+                ]
+        )
+    }
+
+    @objc public func openSurvey(_ call: CAPPluginCall)  {
+
+        if let bridgeViewController = bridge?.viewController as? CAPBridgeViewController {
+            Qualtrics.shared.evaluateProject { (targetingResults) in
+                for (interceptID, result) in targetingResults {
+                    if result.passed() {
+                        call.resolve()
+                        let displayed = Qualtrics.shared.display(viewController: bridgeViewController)
+
+                    }
+                }
+            }
+        } else {
+            call.reject("No se pudo acceder al CAPBridgeViewController")
+        }
     }
 }
